3ae522b2e78301a9f86d4ecde82fed96760cd0ec
Clarice Bouwer <cbillowes@gmail.com>
2022-04-19 07:55:55 +0400

    include proptypes for Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Title = ({ children }) => {
   return <h1>{children}</h1>;
 };
 
 export default Title;
+
+Title.propTypes = {
+  children: PropTypes.node.isRequired,
+};
